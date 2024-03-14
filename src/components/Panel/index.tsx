2d05d5bb9804fdd11515af3599b80047805feb23
Nicolas Jiménez <nicolas.jimenez@edunext.co>
2024-03-14 15:30:43 -0500

    fix: Broken emoji and load render

--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -1,29 +1,19 @@
 import { useContext } from 'react';
-import { motion } from 'framer-motion';
 
 import { Emoji } from '@components/index';
 import { EmojiContext } from '@context/index';
-
-const variants = {
-  initial: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 }
-  }
-};
 
 const Panel = () => {
   const { emojis } = useContext(EmojiContext);
 
   return (
-    <motion.section
+    <section
       className="flex flex-wrap justify-center w-full px-4 mt-2"
-      variants={variants}
-      initial="initial"
-      animate="animate"
     >
       {emojis.map(({ emoji }) => (
         <Emoji emoji={emoji} key={emoji} />
       ))}
-    </motion.section>
+    </section>
   );
 };
 
